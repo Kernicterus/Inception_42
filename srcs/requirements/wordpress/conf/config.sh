72e8218858f47771096a8e6acea6955af4cad976
Kernicterus <149159187+Kernicterus@users.noreply.github.com>
2024-07-12 17:04:45 +0200

    inception

---
+++ srcs/requirements/wordpress/conf/config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sleep 10
+/usr/sbin/php-fpm7.3 -F
+
+wp config create --allow-root \
+	--dbname=$SQL_DATABASE \
+	--dbuser=$SQL_USER \
+	--dbpass=$SQL_PASSWORD \
+	--dbhost=mariadb::3306 --path='/var/www/wordpress'
+
+wp core install --title=Inception \
+	--admin_user=nledent \
+	--admin_email=nledent@inception.com
+
+wp user create nledent2 nledent2@inception.com
