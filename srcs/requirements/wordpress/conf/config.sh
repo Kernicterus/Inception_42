26bac6eae30cccf019166a21ebbb9257fe83e9c1
Nico <nledent@42angouleme.fr>
2024-07-14 20:38:55 +0200

    mandatory ok

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 sleep 10
-php-fpm7.3 -F
+cd /var/www/wordpress
 
+if ! [ -e wp-config.php ]; then
+wp core download --allow-root
 wp config create --allow-root \
 	--dbname=$SQL_DATABASE \
 	--dbuser=$SQL_USER \
@@ -10,8 +12,16 @@ wp config create --allow-root \
 	--dbhost=mariadb:3306 \
 	--path='/var/www/wordpress'
 
-wp core install --title=Inception \
-	--admin_user=nledent \
-	--admin_email=nledent@inception.com
+wp core install --url=$WP_URL \
+	--title=$WP_TITLE \
+	--admin_user=$WP_ADMIN \
+	--admin_password=$WP_ADMIN_PWD \
+	--admin_email=$WP_ADMIN_EMAIL \
+	--allow-root
 
-wp user create nledent2 nledent2@inception.com
+wp user create nledent2 nledent2@inception.com \
+	--role=subscriber --user_pass=$WP_USER_PASSWORD --allow-root
+chmod -R 775 wp-content
+fi
+
+php-fpm7.3 -F
